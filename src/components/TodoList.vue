343f52d4dbcc3abab6e184b4a18cfb6686c583f8
zendzo <zendzzo@gmail.com>
2020-10-02 01:00:21 +0700

    refactoring to sprate component and practice to use component comunication

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,17 +3,13 @@
 		<input type="text" placeholder="What need to be done" class="todo-input" v-model="newTodo" @keyup.enter="addTodo">
 		<!-- annimatie tobe here -->
 		<transition-group tag="div" name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
-		<div v-for="(todo, index) in todosFiltered" :key="todo.id" class="todo-item">
-			<div class="todo-item-left">
-				<input type="checkbox" v-model="todo.completed">
-				<div class="todo-item-label" :class="{completed : todo.completed }" @dblclick="editTodo(todo)" v-if="!todo.editing">{{ todo.title }}</div>
-				<input class="todo-item-edit" v-else type="text" v-model="todo.title"
-				@blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
-			</div>
-			<div class="remove-item" @click="removeItem(index)">
-				&times;
-			</div>
-		</div>
+		<TodoItem v-for="(todo, index) in todosFiltered" 
+		:key="todo.id" 
+		:todo="todo" 
+		:index="index" 
+		:checkAll="!anyRemaining"	
+		@removedTodo="removeTodo"
+		@finishedEdit="finishedEdit"></TodoItem>
 		</transition-group>
 		<div class="extra-container">
 			<div>
@@ -38,8 +34,13 @@
 </template>
 
 <script>
+import TodoItem from "./TodoItem"
+
 export default {
   name: 'TodoList',
+	components: {
+		TodoItem,
+	},
   data () {
     return {
 	  newTodo: '',
@@ -84,13 +85,6 @@ export default {
 		  return this.todos.filter((todo) => todo.completed).length > 0
 	  }
   },
-  directives: {
-	  focus:{
-		  inserted: function(el){
-			  el.focus()
-		  }
-	  }
-  },
   methods: {
 	  addTodo(){
 		  if(this.newTodo.trim().length == 0){
@@ -104,29 +98,18 @@ export default {
 		  this.newTodo = ''
 		  this.idForTodo++
 	  },
-	  removeItem(index){
+	  removeTodo(index){
 		  this.todos.splice(index,1)
-	  },
-	  editTodo(todo){
-		  this.beforeEditCache = todo.title
-		  todo.editing = true
-	  },
-	  doneEdit(todo){
-		  if(todo.title.trim() == ''){
-			  todo.title = this.beforeEditCache
-		  }
-		  todo.editing = false
-	  },
-	  cancelEdit(todo){
-		  todo.title = this.beforeEditCache
-		  todo.editing = false
 	  },
 	  checkAllTodos(){
 		  this.todos.forEach((todo) => todo.completed = event.target.checked)
 	  },
 	  clearCompleted(){
 		  this.todos = this.todos.filter(todo => !todo.completed)
-	  }
+		},
+		finishedEdit(data){
+			this.todos.splice(data.index,1,data.todo)
+		}
   }
 }
 </script>
